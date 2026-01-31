74a21d8cae967e453dcfdbae4d14fe8cd4f994f8
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-01-31 15:34:05 -0300

    ✨ Animação da hero

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ ScrollSmoother.create({
 	smooth: 1.5,
 	effects: true
 });
+
+gsap.from("picture:nth-child(2)",{
+    opacity: 0,
+    duration: 1
+})
+
+gsap.from("picture:nth-child(1)",{
+    y: -60,
+    duration: 1
+})
+
+gsap.from("picture:nth-child(2)",{
+    y: 200,
+    duration: 1
+})
